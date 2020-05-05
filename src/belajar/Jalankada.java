934a3e06ef1b0bc55bf1d3074d365e73b9c7d3ae
afrian22 <62339501+afrian22@users.noreply.github.com>
2020-05-05 13:52:10 +0800

    jadiiii anjayyy

---
+++ src/belajar/Jalankada.java
@@ -0,0 +1,14 @@
+package belajar;
+
+public class Jalankada {
+    public static void main(String[] args) {
+        Laptop mudah = new Laptop();
+        System.out.println(mudah.type = "VivoBook");
+        System.out.println(mudah.merk = "ASUS");
+        System.out.println(mudah.warna = "Merah");
+        System.out.println(mudah.harga);
+        System.out.println(mudah.pembuatan);
+
+        mudah.gaya();
+    }
+}
