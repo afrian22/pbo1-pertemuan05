6281cb548884a3314bcb2abc30d29f1e62351501
afrian22 <62339501+afrian22@users.noreply.github.com>
2020-05-05 13:46:26 +0800

    jadiiii

---
+++ src/belajar/Laptop.java
@@ -0,0 +1,16 @@
+package belajar;
+
+public class Laptop {
+    String type;
+    String merk;
+    String warna;
+    int harga = 2000000;
+    final int pembuatan = 2019;
+
+    void gaya() {
+        System.out.println("Mudah Dibawa Kemana-mana");
+        System.out.println("Menggunakan Batray");
+        System.out.println("Bisa digunakan Kapanpun Selama ada Batray");
+
+    }
+}
